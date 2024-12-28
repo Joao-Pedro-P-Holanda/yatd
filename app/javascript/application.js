2b3ee65fd31cef3e69e80748d5bb624699a6184a
joao <joao@localhost.localdomain>
2024-12-28 09:43:14 -0300

    chore: adding scroll to top button
    adding component scroll to top
    
    adding javascript logic to hide button when page scroll is less than a
    given threshold and scroll to top when clicking the button
    
    overriding bootstrap button styling and styling back to top button fixed
    on right border

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,22 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 import * as bootstrap from "bootstrap"
+
+document.addEventListener("DOMContentLoaded", function() {
+  const backToTopButton = document.getElementById("back-to-top");
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 200) {
+      backToTopButton.style.display = "block";
+    } else {
+      backToTopButton.style.display = "none";
+    }
+  });
+  // using scroll instead of the href to trigger the scroll event
+  backToTopButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  });
+})
